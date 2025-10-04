76d4d4bcb942e173934f60a0145fe3994bdb749c
ghbrjks <shadow.pixel47@gmail.com>
2025-10-04 16:45:10 +0300

    3l

--- l3/task2/task2.py
+++ l3/task2/task2.py
@@ -1,12 +1,12 @@
 ﻿
-PROPS = ['color', 'nationality', 'drink', 'cigarette', 'pet']
+PROPS = ['цвет', 'национальность', 'напиток', 'сигареты', 'животное']
 
 DOMAINS = {
-    'color':      ['red', 'green', 'white', 'yellow', 'blue'],
-    'nationality':['norwegian', 'english', 'dane', 'german', 'swede'],
-    'drink':      ['tea', 'coffee', 'milk', 'water', 'beer'],
-    'cigarette':  ['marlboro', 'dunhill', 'rothmans', 'pall_mall', 'winfield'],
-    'pet':        ['cats', 'birds', 'dogs', 'horses', 'fish']
+    'цвет':      ['красный', 'зеленый', 'белый', 'желтый', 'синий'],
+    'национальность':['норвежец', 'англичанин', 'датчанин', 'немец', 'швед'],
+    'напиток':      ['чай', 'кофе', 'молоко', 'вода', 'пиво'],
+    'сигареты':  ['мальборо', 'данхилл', 'ротманс', 'поллмолл', 'винфилд'],
+    'животное':        ['кошки', 'птицы', 'собаки', 'лошади', 'рыбки']
 }
 
 houses = [ {p: None for p in PROPS} for _ in range(5) ]
@@ -51,6 +51,8 @@ def neighbor_constraint(houses, keyA, valA, keyB, valB):
     if posA is not None and posB is not None:
         if abs(posA - posB) == 1:
             return True
+        else:
+            return False
 
     # если A известен, проверяем, что его сосед может быть B
     if posA is not None and posB is None:
@@ -76,10 +78,10 @@ def neighbor_constraint(houses, keyA, valA, keyB, valB):
 
     return True
 
-def left_of_constraint(houses, left_color, right_color):
+def left_of_constraint(houses, left_цвет, right_цвет):
     # проверка позиции домов по цветам
-    posL = find_pos(houses, 'color', left_color)
-    posR = find_pos(houses, 'color', right_color)
+    posL = find_pos(houses, 'цвет', left_цвет)
+    posR = find_pos(houses, 'цвет', right_цвет)
 
     # проверка крайних позиций домов
     if posL is not None and posL == 4:
@@ -94,12 +96,12 @@ def left_of_constraint(houses, left_color, right_color):
     # проверки на соответствие цветов соседних домов
     if posL is not None and posR is None:
         right_idx = posL + 1
-        if houses[right_idx]['color'] is not None and houses[right_idx]['color'] != right_color:
+        if houses[right_idx]['цвет'] is not None and houses[right_idx]['цвет'] != right_цвет:
             return False
 
     if posR is not None and posL is None:
         left_idx = posR - 1
-        if houses[left_idx]['color'] is not None and houses[left_idx]['color'] != left_color:
+        if houses[left_idx]['цвет'] is not None and houses[left_idx]['цвет'] != left_цвет:
             return False
 
     return True
@@ -117,70 +119,70 @@ def is_valid(houses):
         return False
 
     # 1. Норвежец живёт в первом доме.
-    if houses[0]['nationality'] is not None and houses[0]['nationality'] != 'norwegian':
+    if houses[0]['национальность'] is not None and houses[0]['национальность'] != 'норвежец':
         return False
-    pos_nor = find_pos(houses, 'nationality', 'norwegian')
+    pos_nor = find_pos(houses, 'национальность', 'норвежец')
     if pos_nor is not None and pos_nor != 0:
         return False
 
     # 2. Англичанин живёт в красном доме.
-    if not same_house_constraint(houses, 'nationality', 'english', 'color', 'red'):
+    if not same_house_constraint(houses, 'национальность', 'англичанин', 'цвет', 'красный'):
         return False
 
     # 3. Зелёный дом находится слева от белого, рядом с ним.
-    if not left_of_constraint(houses, 'green', 'white'):
+    if not left_of_constraint(houses, 'зеленый', 'белый'):
         return False
 
     # 4. Датчанин пьёт чай.
-    if not same_house_constraint(houses, 'nationality', 'dane', 'drink', 'tea'):
+    if not same_house_constraint(houses, 'национальность', 'датчанин', 'напиток', 'чай'):
         return False
 
-    # 5. Тот, кто курит Marlboro, живёт рядом с тем, кто выращивает кошек.
-    if not neighbor_constraint(houses, 'cigarette', 'marlboro', 'pet', 'cats'):
+    # 5. Тот, кто курит мальборо, живёт рядом с тем, кто выращивает кошек.
+    if not neighbor_constraint(houses, 'сигареты', 'мальборо', 'животное', 'кошки'):
         return False
 
-    # 6. Тот, кто живёт в жёлтом доме, курит Dunhill.
-    if not same_house_constraint(houses, 'color', 'yellow', 'cigarette', 'dunhill'):
+    # 6. Тот, кто живёт в жёлтом доме, курит данхилл.
+    if not same_house_constraint(houses, 'цвет', 'желтый', 'сигареты', 'данхилл'):
         return False
 
-    # 7. Немец курит Rothmans.
-    if not same_house_constraint(houses, 'nationality', 'german', 'cigarette', 'rothmans'):
+    # 7. Немец курит ротманс.
+    if not same_house_constraint(houses, 'национальность', 'немец', 'сигареты', 'ротманс'):
         return False
 
     # 8. Тот, кто живёт в центре, пьёт молоко.
-    if houses[2]['drink'] is not None and houses[2]['drink'] != 'milk':
+    if houses[2]['напиток'] is not None and houses[2]['напиток'] != 'молоко':
         return False
-    pos_milk = find_pos(houses, 'drink', 'milk')
-    if pos_milk is not None and pos_milk != 2:
+    pos_молоко = find_pos(houses, 'напиток', 'молоко')
+    if pos_молоко is not None and pos_молоко != 2:
         return False
 
-    # 9. Сосед того, кто курит Marlboro, пьёт воду.
-    if not neighbor_constraint(houses, 'cigarette', 'marlboro', 'drink', 'water'):
+    # 9. Сосед того, кто курит мальборо, пьёт воду.
+    if not neighbor_constraint(houses, 'сигареты', 'мальборо', 'напиток', 'вода'):
         return False
 
     # 10. Тот, кто курит Pall Mall, выращивает птиц.
-    if not same_house_constraint(houses, 'cigarette', 'pall_mall', 'pet', 'birds'):
+    if not same_house_constraint(houses, 'сигареты', 'поллмолл', 'животное', 'птицы'):
         return False
 
     # 11. Швед выращивает собак.
-    if not same_house_constraint(houses, 'nationality', 'swede', 'pet', 'dogs'):
+    if not same_house_constraint(houses, 'национальность', 'швед', 'животное', 'собаки'):
         return False
 
     # 12. Норвежец живёт рядом с синим домом.
-    if not neighbor_constraint(houses, 'nationality', 'norwegian', 'color', 'blue'):
+    if not neighbor_constraint(houses, 'национальность', 'норвежец', 'цвет', 'синий'):
         return False
 
     # 13. Тот, кто выращивает лошадей, живёт в синем доме.
-    # (двунаправленное: blue <-> horses)
-    if not same_house_constraint(houses, 'pet', 'horses', 'color', 'blue'):
+    # (двунаправленное: синий <-> лошади)
+    if not same_house_constraint(houses, 'животное', 'лошади', 'цвет', 'синий'):
         return False
 
-    # 14. Тот, кто курит Winfield, пьет пиво.
-    if not same_house_constraint(houses, 'cigarette', 'winfield', 'drink', 'beer'):
+    # 14. Тот, кто курит винфилд, пьет пиво.
+    if not same_house_constraint(houses, 'сигареты', 'винфилд', 'напиток', 'пиво'):
         return False
 
     # 15. В зелёном доме пьют кофе.
-    if not same_house_constraint(houses, 'color', 'green', 'drink', 'coffee'):
+    if not same_house_constraint(houses, 'цвет', 'зеленый', 'напиток', 'кофе'):
         return False
 
     return True
@@ -237,11 +239,49 @@ def query():
         else:
             print("Не найдено дома с таким значением.")
 
+def prolog_like_query():
+    print("\nДоступные свойства:", PROPS)
+    while True:
+        known_keys = {}
+        target_keys = {}
+        dict_keys = list(DOMAINS.values())
+        for word in dict_keys:
+            print(word)
+        request = input(f"Введите запрос: ").replace(" ","").lower().split(",")
+        for word_req in request:
+            for i in range(5):
+                if word_req == "_":
+                    continue
+                elif word_req == dict_keys[i]:
+                    index = request.index(word_req)
+                    known_keys[index] = word_req
+                else:
+                    index = request.index(word_req)
+                    target_keys[index] = word_req
+        keys_k = list(known_keys.keys())
+        values_k = list(known_keys.values())
+        keys_t = list(target_keys.keys())
+        values_t = list(target_keys.values())
+        print("Индекс известных ключей")
+        for word in keys_k:
+            print("!",word,"!")
+        print("Значение известных ключей")
+        for word in values_k:
+            print("!",word,"!")
+        print("Индекс искомых ключей")
+        for word in keys_t:
+            print(word)
+        print("Значение искомых ключей")
+        for word in values_t:
+            print(word)
+
+
+
 def print_solution(sol):
-    print("{:>2} {:>8} {:>12} {:>8} {:>12} {:>8}\n".format("№","color","nationality","drink","cigarette","pet"))
+    print("{:>2} {:>8} {:>12} {:>8} {:>12} {:>8}\n".format("№","цвет","национальность","напиток","сигареты","животное"))
     for i,h in enumerate(sol):
         print("{:2d} {:>8} {:>12} {:>8} {:>12} {:>8}".format(
-            i+1, h['color'], h['nationality'], h['drink'], h['cigarette'], h['pet']
+            i+1, h['цвет'], h['национальность'], h['напиток'], h['сигареты'], h['животное']
         ))
 
 if __name__ == "__main__":
@@ -250,4 +290,4 @@ if __name__ == "__main__":
         print("Решений не найдено.")
     else:
         print_solution(solution)
-        query()
+        prolog_like_query()
